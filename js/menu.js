e07081649792c2d79977ea70dd4e766e54e7954e
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-07-23 13:50:43 +0200

    Add header background

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,8 @@
-document.write('<div id="nav"><ul>\
-<li><a class="active" href="#home">Home</a></li>\
-<li><a href="#news">News</a></li>\
-<li><a href="#contact">Contact</a></li>\
-<li><a href="#about">About</a></li>\
-</ul></div>\
+document.write('<div id="nav">\
+<div id="login_info"><div id="inside_login_box"> Logged as: User</div></div>\
+<div id="inside_menu"> <ul><li><a class="active" href="#home">STORE</a></li>\
+<li><a href="#about">PROFILE</a></li></ul></div>\
+<div id="level_info"><div id="inside_login_box"> Level/money 50 / 1.000<br>\
+    </div></div>\
+</div>\
 ')
