956088cfaf64470fc91f175da97771b5a523a066
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-07-24 14:00:20 +0200

    Template of profile site

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-document.write('<div id="nav">\
+document.write('<nav>\
 <div id="left_menu_item"><div id="menu_item_inside"> Logged as: User</div></div>\
 <div id="center_menu_item"> <ul><li><a class="active" href="#home">STORE</a></li>\
-<li><a href="#about">PROFILE</a></li></ul></div>\
+<li><a href="profile.html">PROFILE</a></li></ul></div>\
 <div id="right_menu_item"><div id="menu_item_inside">  50 <img id="menu_icon" src="img/coin.png" width=20px> 1.000 <img id="menu_icon" src="img/lvl.png" width=20px>\
     </div></div>\
-</div>\
+</nav>\
 ')
