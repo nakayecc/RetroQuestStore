597949af0d32497c4b2703944d8897aedcd36b3d
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-07-24 09:22:57 +0200

    Adjust menu content

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ document.write('<div id="nav">\
 <div id="left_menu_item"><div id="menu_item_inside"> Logged as: User</div></div>\
 <div id="center_menu_item"> <ul><li><a class="active" href="#home">STORE</a></li>\
 <li><a href="#about">PROFILE</a></li></ul></div>\
-<div id="right_menu_item"><div id="menu_item_inside"> Level/money 50 / 1.000<br>\
+<div id="right_menu_item"><div id="menu_item_inside"> Level: 50<br> Money: 1.000<br>\
     </div></div>\
 </div>\
 ')
