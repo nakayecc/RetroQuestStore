bc60670e9d1b6a5b86078a66027c62585fd20ad9
David <cichy.dawid@gmail.com>
2019-07-25 10:36:47 +0200

    Added Mentors Menu

---
+++ js/menu_mentor.js
@@ -0,0 +1,16 @@
+document.write('<nav>\
+    <div id = "left_menu_item">\
+        <div id="menu_item_inside"> Logged as: User</div >\
+    </div >\
+    <div id="center_menu_item">\
+        <ul>\
+            <li><a href="mentor_users.html">USERS</a></li>\
+            <li><a href="mentor_quests.html">QUESTS</a></li >\
+            <li><a href="mentor_items.html">ITEMS</a></li >\
+        </ul >\
+    </div >\
+    <div id="right_menu_item">\
+        <div id="menu_item_inside">  50 <img id="menu_icon" src="img/coin.png" width="20px"> 1.000 <img id="menu_icon" src="img/lvl.png" width="20px"></div>\
+    </div>\
+</nav >\
+    ')
